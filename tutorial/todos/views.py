b0822d4a6b14395288389e06981dbc3c5ff3e2f8
Alex <Like2968@gmail.com>
2025-07-17 16:16:59 +0800

    feat(todos): add show_student action to TeacherViewSet for retrieving associated students

--- tutorial/todos/views.py
+++ tutorial/todos/views.py
@@ -1,11 +1,13 @@
 '''
-檔案:views.py
+檔案: views.py
 作者: Alex LIN
 功能: 建立Teacher, Student的API接口，及對應的CRUD操作
 '''
 
 # third-party
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 #app
 from .models import Teachers, Students
@@ -19,3 +21,10 @@ class StudentViewSet(viewsets.ModelViewSet):
 class TeacherViewSet(viewsets.ModelViewSet):
     queryset = Teachers.objects.all()
     serializer_class = TeacherSerializer
+
+    @action(detail=True, methods=['get'], url_path='showStudent')
+    def show_student(self, request, pk=None):
+        teacher = self.get_object()
+        students = teacher.students.all()  # 使用 related_name='students'
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data)
