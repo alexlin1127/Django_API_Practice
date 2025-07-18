98b653420c799fc219d9d39a6f37dcc0724c63e1
Alex <Like2968@gmail.com>
2025-07-18 14:23:37 +0800

    feat(todos): add image field to students model in migration 0009

--- tutorial/todos/models.py
+++ tutorial/todos/models.py
@@ -21,6 +21,8 @@ class Students(models.Model):
     email = models.EmailField(unique=True)
     grade = models.IntegerField()
     enrolled_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='student_images/', blank=True, null=True)
+
 
     teacher = models.ForeignKey(
         Teachers,
